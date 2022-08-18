b105f87d988d84f5a618efa4ba16dc777a6f6347
Anbu <mugilbala@gmail.com>
2022-08-17 21:51:09 -0400

    first commit

---
+++ src/test/java/com/vetha/space/data_structures/DataStructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetha.space.data_structures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
